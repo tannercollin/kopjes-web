6532aa04d1966560326a525da47338b27464267e
Tanner Collin <git@tannercollin.com>
2018-05-13 16:12:40 -0600

    Install skeleton-css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-		<p>Hello world</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>My big header</h1>
+				<p>Hello world</p>
+			</div>
+		);
+	}
 }
 
 export default App;
